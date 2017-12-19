cc2582ea888c6fd986388adb4e130b1f5e26ea2d
Ivan Magda <imagda15@gmail.com>
2017-12-20 02:17:35 +0300

    [IMYouTube] feat: Create dummy RecyclerView

---
+++ IMYouTube/app/src/main/java/com/ivanmagda/imyoutube/adapters/MainAdapter.kt
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2017 Ivan Magda
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.ivanmagda.imyoutube.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ivanmagda.imyoutube.R
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class MainAdapter : RecyclerView.Adapter<ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent?.context)
+        val view = layoutInflater.inflate(R.layout.list_item, parent, false)
+
+        view.tv_list_item.text = "123"
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return 4
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+    }
+}
+
+class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
