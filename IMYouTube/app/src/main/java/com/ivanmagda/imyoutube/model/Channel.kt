f49c7e7b37a6ae80718808106072a4dd3d04af41
Ivan Magda <imagda15@gmail.com>
2017-12-30 19:17:04 +0300

    [IMYouTube] chore: Support Parcelable for Channel

--- IMYouTube/app/src/main/java/com/ivanmagda/imyoutube/model/Channel.kt
+++ IMYouTube/app/src/main/java/com/ivanmagda/imyoutube/model/Channel.kt
@@ -22,7 +22,35 @@
 
 package com.ivanmagda.imyoutube.model
 
-class Channel(val name: String, val profileImageUrl: String, val numberOfSubscribers: Int) {
+import android.os.Parcel
+import android.os.Parcelable
+
+class Channel(val name: String, val profileImageUrl: String, val numberOfSubscribers: Int) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readInt())
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(profileImageUrl)
+        parcel.writeInt(numberOfSubscribers)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Channel> {
+        override fun createFromParcel(parcel: Parcel): Channel {
+            return Channel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Channel?> {
+            return arrayOfNulls(size)
+        }
+    }
 
     override fun toString(): String {
         return "Channel(name='$name', profileImageUrl='$profileImageUrl', numberOfSubscribers=$numberOfSubscribers)"
