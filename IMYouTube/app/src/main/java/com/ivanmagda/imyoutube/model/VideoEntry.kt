693d7169dd6f75d64b57dfbe2d74c9603fa5496d
Ivan Magda <imagda15@gmail.com>
2017-12-30 19:33:10 +0300

    [IMYouTube] chore: Create VideoEntry model object

---
+++ IMYouTube/app/src/main/java/com/ivanmagda/imyoutube/model/VideoEntry.kt
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2017 Ivan Magda
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.ivanmagda.imyoutube.model
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class VideoEntry(val name: String, val duration: String, val number: Int, val imageUrl: String,
+                 val link: String) : Parcelable {
+
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readInt(),
+            parcel.readString(),
+            parcel.readString())
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(duration)
+        parcel.writeInt(number)
+        parcel.writeString(imageUrl)
+        parcel.writeString(link)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<VideoEntry> {
+        override fun createFromParcel(parcel: Parcel): VideoEntry {
+            return VideoEntry(parcel)
+        }
+
+        override fun newArray(size: Int): Array<VideoEntry?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
