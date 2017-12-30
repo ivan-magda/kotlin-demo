a978c2db998f7fba9452e9a15412f237a360010e
Ivan Magda <imagda15@gmail.com>
2017-12-30 17:44:50 +0300

    [IMYouTube] chore: Create DetailActivity

---
+++ IMYouTube/app/src/main/java/com/ivanmagda/imyoutube/activities/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.ivanmagda.imyoutube.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.ivanmagda.imyoutube.R
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
