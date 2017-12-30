ee3eef7f219e43de7dced7369dbee0f7e6e45d2d
Ivan Magda <imagda15@gmail.com>
2017-12-30 14:56:52 +0300

    [IMYouTube] feat: Load and cache images

--- IMYouTube/app/src/main/java/com/ivanmagda/imyoutube/adapters/HomeFeedAdapter.kt
+++ IMYouTube/app/src/main/java/com/ivanmagda/imyoutube/adapters/HomeFeedAdapter.kt
@@ -29,6 +29,7 @@ import android.view.ViewGroup
 import com.ivanmagda.imyoutube.R
 import com.ivanmagda.imyoutube.model.HomeFeed
 import com.ivanmagda.imyoutube.model.Video
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.list_item.view.*
 
 class HomeFeedAdapter(val homeFeed: HomeFeed = HomeFeed()) : RecyclerView.Adapter<HomeFeedViewHolder>() {
@@ -51,6 +52,11 @@ class HomeFeedAdapter(val homeFeed: HomeFeed = HomeFeed()) : RecyclerView.Adapte
 
 class HomeFeedViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
     fun configure(video: Video) {
-        view.tv_list_item.text = video.name
+        view.tv_list_item_video.text = video.name
+        view.tv_list_item_channel.text = video.channel.name
+
+        val picasso = Picasso.with(view.context)
+        picasso.load(video.imageUrl).into(view.iv_list_item_video)
+        picasso.load(video.channel.profileImageUrl).into(view.iv_list_item_profile)
     }
 }
